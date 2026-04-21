295080d3a610fe18dc36046c64712406469a2327
BIPIN0852 <bipinaryal0852@gmail.com>
2026-04-21 09:27:05 +0545

    updated server.js

--- backend/server.js
+++ backend/server.js
@@ -709,6 +709,7 @@ app.use(
 const allowedOrigins = [
   "http://localhost:5173",
   "http://localhost:3000",
+  "https://pharmacy-management-system-peach.vercel.app", // Explicitly added your live Vercel URL
   process.env.FRONTEND_URL, // Ensure you add FRONTEND_URL in Render env
 ];
 
